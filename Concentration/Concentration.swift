d52e4c36790817bfeec78c79510198d0bb278e39
miuchena <ana.techon@gmail.com>
2018-03-25 13:31:17 +0300

    Code from lecture 3

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -9,33 +9,52 @@
 import Foundation
 
 class Concentration {
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyFaced: Int?
+    private var indexOfOneAndOnlyFacedUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFacedUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+                set {
+                    for index in cards.indices {
+                        cards[index].isFacedUp = (index == newValue)
+                    }
+                }
+            }
+
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
-            if let matchIndex = indexOfOneAndOnlyFaced, matchIndex != index {
+            if let matchIndex = indexOfOneAndOnlyFacedUpCard, matchIndex != index {
                 // check if cards match
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
                 cards[index].isFacedUp = true
-                indexOfOneAndOnlyFaced = nil
+//                indexOfOneAndOnlyFaced = nil
             } else {
                 // either no cards or 2 cards are faced up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFacedUp = false
-                }
-                cards[index].isFacedUp = true
-                indexOfOneAndOnlyFaced = index
+ //               for flipDownIndex in cards.indices {
+   //                 cards[flipDownIndex].isFacedUp = false
+                indexOfOneAndOnlyFacedUpCard = index
+               
             }
         }
     }
     
     init(numberOfPairsOfCards: Int) {
-        for _ in 1...numberOfPairsOfCards {
+        for _ in 0..<numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
         }
